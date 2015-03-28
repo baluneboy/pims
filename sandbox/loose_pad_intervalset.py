d00e1d2b50115421cbc3efed29f7d76fde4d804a
ken <ken@macmini3.local>
2015-03-28 10:21:06 -0400

    Renamed and stripped loose_pad_intervalset.py.

---
+++ sandbox/loose_pad_intervalset.py
@@ -0,0 +1,112 @@
+#! /usr/bin/env python
+
+import datetime
+from interval import Interval, IntervalSet
+
+class LoosePadIntervalSet(IntervalSet):
+
+    def __init__(self, items=[], maxgapsec=1.5):
+        "Initializes the LoosePadIntervalSet"
+        if items:
+            if not all(isinstance(x, datetime.datetime) for x in items):
+                raise TypeError('items must all be datetime type')
+        super(LoosePadIntervalSet, self).__init__(items)
+        self.maxgapsec = maxgapsec
+
+    def close_enough(self, i1, i2):
+        """Tells whether 2 pad intervals are close enough to each other
+
+        Returns True if i1 is close enough to (within maxgapsec seconds of) i2
+        meaning if they were joined, then there would be no discontinuity.
+
+        >>> d1 = datetime.datetime(2015,1,1,0,0,0)
+        >>> d2 = datetime.datetime(2015,1,1,0,0,1,100000)
+        >>> d3 = datetime.datetime(2015,1,1,0,0,2)
+        >>> d4 = datetime.datetime(2015,1,1,0,0,6,600000)
+        >>> d5 = datetime.datetime(2015,1,1,0,0,8)
+        >>> d6 = datetime.datetime(2015,1,1,0,0,8,876543)
+        >>> r1 = Interval(d1, d2)
+        >>> r2 = Interval(d3, d4)
+        >>> r3 = Interval(d5, d6)
+        >>> s = LoosePadIntervalSet()
+        >>> s.close_enough(r1, r2)
+        True
+        >>> s.close_enough(r2, r3)
+        True
+        >>> s.close_enough(r1, r3)
+        False
+
+        """
+        if i1.comes_before(i2):
+            if (i2.lower_bound - i1.upper_bound).total_seconds() <= self.maxgapsec:
+                result = True
+            else:
+                result = False
+        elif i1 == i2:
+            result = True
+        else:
+            result = self.close_enough(i2, i1)
+        return result
+
+    def _get_fill(self, i1, i2):
+        if i1.comes_before(i2):
+            result = Interval(i1.upper_bound, i2.lower_bound)
+        else:
+            result = Interval(i2.upper_bound, i1.lower_bound)
+        return result        
+
+    def add(self, obj):
+        """Adds an Interval or discrete value to the object
+
+        >>> d0 = datetime.datetime(2014,12,31,23,59,58)
+        >>> d1 = datetime.datetime(2015,1,1,0,0,0)
+        >>> d2 = datetime.datetime(2015,1,1,0,0,1,100000)
+        >>> d3 = datetime.datetime(2015,1,1,0,0,2)
+        >>> d4 = datetime.datetime(2015,1,1,0,0,6,600000)
+        >>> d5 = datetime.datetime(2015,1,1,0,0,8,100000)
+        >>> d6 = datetime.datetime(2015,1,1,0,0,8,876543)
+        >>> r1 = Interval(d1, d2)
+        >>> r2 = Interval(d3, d4)
+        >>> r3 = Interval(d5, d6)        
+        >>> r = LoosePadIntervalSet()
+        >>> r.add(d0)
+        >>> print r
+        datetime.datetime(2014, 12, 31, 23, 59, 58)
+        >>> r.add(r1)
+        >>> print r
+        datetime.datetime(2014, 12, 31, 23, 59, 58),[datetime.datetime(2015, 1, 1, 0, 0)..datetime.datetime(2015, 1, 1, 0, 0, 1, 100000)]
+        >>> r.add(r2)
+        >>> print r
+        datetime.datetime(2014, 12, 31, 23, 59, 58),[datetime.datetime(2015, 1, 1, 0, 0)..datetime.datetime(2015, 1, 1, 0, 0, 6, 600000)]
+        >>> r.add(r3)
+        >>> print r
+        datetime.datetime(2014, 12, 31, 23, 59, 58),[datetime.datetime(2015, 1, 1, 0, 0)..datetime.datetime(2015, 1, 1, 0, 0, 8, 876543)]
+
+        """
+        if isinstance(obj, Interval):
+            r = obj
+        else:
+            r = Interval.equal_to(obj)
+        
+        if r:   # Don't bother appending an empty Interval
+            # If r continuously joins with any of the other
+            if not (isinstance(r.lower_bound, datetime.datetime) and isinstance(r.upper_bound, datetime.datetime)):
+                raise TypeError('cannot add non-datetime interval or object')
+                
+            newIntervals = []
+            for i in self.intervals:
+                if i.overlaps(r) or i.adjacent_to(r):
+                    r = r.join(i)
+                elif self.close_enough(i, r):
+                    gap_int = self._get_fill(i, r)
+                    r = r.join(gap_int) # spackle the gap!
+                    r = r.join(i)
+                else:
+                    newIntervals.append(i)
+            newIntervals.append(r)
+            self.intervals = newIntervals
+            self.intervals.sort()
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
