fdeee1cca70c38cac7b1527a4ad4a86e7dc99ed8
ken <ken@macmini3.local>
2015-04-02 18:22:11 -0400

    Homework clean up dsm_status_check.py.

--- pad/dsm_status_check.py
+++ pad/dsm_status_check.py
@@ -13,22 +13,16 @@ def get_packets_timedelta(sensor, start, stop, host, packets_per_second=8):
     m = r[0][0] / packets_per_second / 60.0
     return datetime.timedelta( minutes=m )
 
-#def get_db_minutes(x):
-#    if not x[2]: return None
-#    t1 = str(x[1])
-#    t2 = str(x[2])
-#    return get_packets_timedelta('121f03', t1, t2, 'manbearpig')
-
-def get_db_minutes(start, stop):
+# query host's sensor table to calculate amount of packets from start to stop and return that as timedelta
+def get_db_minutes(start, stop, sensor='121f03', host='manbearpig'):
+    """query host's sensor table to calculate amount of packets from start to stop and return that as timedelta"""
     if not start: return None
-    print start, stop
-    return get_packets_timedelta('121f03', str(start), str(stop), 'manbearpig')
+    return get_packets_timedelta(sensor, str(start), str(stop), host)
 
 df = pd.read_csv('/misc/yoda/www/plots/user/sams/gaps/input4dsmstatuschk.csv', sep='\s\s')
 df['start'] = df['start'].apply(doytimestr_to_datetime)
 df['stop'] = df['stop'].apply(doytimestr_to_datetime)
 df['tdelta'] = df['stop'] - df['start']
-#df['dbminutes'] = df.apply(get_db_minutes, axis=1)
-df['dbminutes'] = df.apply(lambda x: get_db_minutes(x['start'], x['stop']), axis=1)
+df['dbminutes'] = df.apply(lambda x: get_db_minutes(x['start'], x['stop'], sensor='121f03', host='manbearpig'), axis=1)
 df['missing'] = df['tdelta'] - df['dbminutes']
 print df
