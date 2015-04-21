f8ce2e849b9a864140cc6c0305cd031b2174c897
pims <pims@park>
2015-04-21 05:21:02 -0400

    For test, use RapidTimeGetter class.

--- largetime/timemachine.py
+++ largetime/timemachine.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 
 import datetime
+from itertools import cycle
 from nose.tools import assert_true, assert_false
 from statemachine import event, Machine, transition_to, transition_from
+from pims.utils.pimsdateutil import unix2dtm
+
+from pims.utils.pimsdateutil import dtm2unix
 
 DEBUG = 1
 
@@ -18,13 +22,24 @@ class TimeGetter(object):
     def get_time(self):
         return None
 
-class NowTimeGetter(TimeGetter):
+class RapidTimeGetter(TimeGetter):
 
     def __init__(self, *args, **kwargs):
-        super(NowTimeGetter, self).__init__(*args, **kwargs)
+        super(RapidTimeGetter, self).__init__(*args, **kwargs)
+        self._init_time = dtm2unix(datetime.datetime(2015,1,1))
+        deltas = [ None, None, None, 0, 1, 2, 3, 3, 3, 4, 5, 6, 4, 4, 4, 5 ]
+        self._pool = cycle(deltas)
     
     def get_time(self):
-        return datetime.datetime.now()
+        delta = self._pool.next()
+        if delta is None:
+            return None
+        return self._init_time + delta
+    
+class NowTimeGetter(TimeGetter):
+    
+    def get_time(self):
+        return dtm2unix( datetime.datetime.now() )
 
 #table = 'es05'
 #host = 'manbearpig'
@@ -33,6 +48,12 @@ class NowTimeGetter(TimeGetter):
 #
 #ntg = NowTimeGetter(None, host=None)
 #print ntg.table, ntg.host, ntg.get_time()
+#
+#rtg = RapidTimeGetter(None, host=None)
+#print rtg.table, rtg.host, rtg.get_time()
+#print '-' * 22
+#for i in range(27):
+#    print rtg.get_time()
 #
 #raise SystemExit
 
@@ -57,7 +78,7 @@ class TimeMachine(Machine):
 
     def __str__(self):
         if self.time:
-            timestr = self.time.strftime('%H:%M:%S')
+            timestr = unix2dtm(self.time).strftime('%H:%M:%S')
         else:
             timestr = 'HH:MM:SS'
         #return 'now the time is %s (%s, %s)' % (timestr, self.state, self.color)
@@ -73,13 +94,11 @@ class TimeMachine(Machine):
         if not old_time:
             self.more_fresh()
             return
-        delta_sec = (new_time - old_time).total_seconds()
+        delta_sec = new_time - old_time
         if delta_sec >= self.expected_delta_sec:
             self.more_fresh()
-            return
         else:
             self.less_fresh()
-            return
 
     @event
     def less_fresh(self):
@@ -93,6 +112,7 @@ class TimeMachine(Machine):
         yield 'rotten', 'stale'
         yield 'stale', 'fresh'
 
+    ###############################
     # FRESH TRANSITIONS
 
     @transition_to('fresh')
@@ -110,6 +130,7 @@ class TimeMachine(Machine):
         self.became_fresh = False
         self.left_fresh = True
 
+    ###############################
     # STALE TRANSITIONS
 
     @transition_to('stale')
@@ -127,6 +148,7 @@ class TimeMachine(Machine):
         self.became_stale = False
         self.left_stale = True
 
+    ###############################
     # ROTTEN TRANSITIONS
 
     @transition_to('rotten')
@@ -155,17 +177,17 @@ class TimeMachine(Machine):
         self.went_stale_when_leaving_rotten = self.state == 'stale'
 
 def demo():
-    tg = NowTimeGetter(None, host=None)
+    tg = RapidTimeGetter(None, host=None)
     print 'START AT', tg.get_time()
     expected_delta_sec = 0.000015
     tm = TimeMachine(tg, expected_delta_sec=expected_delta_sec)
     print tm
-    for i in range(5):
+    for i in range(33):
         tm.update()
         print tm
     
-demo()
-raise SystemExit
+#demo()
+#raise SystemExit
 
 def test_transition_to():
     m = TimeMachine('es05')
