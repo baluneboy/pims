8422a62b0bd2c74add279e1d7d108f43b4e4773e
pims <pims@park>
2015-05-06 16:10:34 -0400

    Reword __init__.py for pims.

--- __init__.py
+++ __init__.py
@@ -2,5 +2,5 @@
 Principal Investigator Microgravity Services (PIMS)
 ---------------------------------------------------
 This package primarily contains microgravity acceleration analysis code.
-You might find parts of it useful for other things (TWSS).
+You might find some parts useful for other things.
 """
