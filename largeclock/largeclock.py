fa743021f961f988ce464496091af06d47563cac
SilverSnoopy2002 <silversnoopy2002@gmail.com>
2015-09-02 11:53:01 +0000

    Added right mouse click to copy timestamp to clipboard.

--- largeclock/largeclock.py
+++ largeclock/largeclock.py
@@ -3,6 +3,7 @@
 """Use Tkinter to show a large digital clock."""
 
 # 8/20/2015 Hrovat modified to allow for input arguments
+# 9/1/2015  Hrovat modified for white bg when yoda db problem
 
 import os
 import sys
@@ -13,7 +14,7 @@ from Tkinter import *
 from multiprocessing import Queue
 
 from pims.largeclock.db import query_aos
-from pims.largeclock.timeout import run_with_timeout
+#from pims.largeclock.timeout import run_with_timeout
 
 _PWORD = raw_input('samsops pword: ') # USE TEMPORARY HARD-CODED FOR PY2EXE
 
@@ -26,23 +27,29 @@ def get_config():
     if len(sys.argv) == 1:
         try:
             # attempt to read local config file
-            script_path = get_script_path()
-            config_file = os.path.join(script_path, "largeclock.cfg")
-            #config_file = r"C:\\largeclock\\largeclock.cfg"
+            #script_path = get_script_path()
+            #config_file = os.path.join(script_path, "largeclock.cfg")
+            config_file = r"C:\\largeclock\\largeclock.cfg"
             config = ConfigParser.ConfigParser()
             config.read(config_file)
             font_size = int( config.get('font', 'size') )
             w = int( config.get('window', 'width') )
             h = int( config.get('window', 'height') )
+            _default = config.get('timeformat', 'default')
+            _custom = config.get('timeformat', 'custom')
         except:
             font_size = 180 # font size
             w, h = 1500, 200 # width and height of Tk root window
+            _default = '%H:%M:%S'
+            _custom = '%Y-%m-%d %H:%M:%S'
     else:
         font_size = int(sys.argv[1])
         w = int(sys.argv[2])
         h = int(sys.argv[3])
-        
-    return font_size, w, h
+        _default = sys.argv[4]
+        _custom = sys.argv[5]
+   
+    return font_size, w, h, _default, _custom
 
 # get query results for Ku AOS/LOS with timeout
 def get_aos_query_results_with_timeout(_PWORD):
@@ -74,8 +81,8 @@ def tick():
         # every 5 seconds check if we are AOS/LOS and
         # update color & title text
         if time2[-1:] in ['0', '5']:
-            #ku_timestamp, ku_aos_los = query_aos(_PWORD)
-            ku_timestamp, ku_aos_los = get_aos_query_results_with_timeout(_PWORD)
+            ku_timestamp, ku_aos_los = query_aos(_PWORD)
+            #ku_timestamp, ku_aos_los = get_aos_query_results_with_timeout(_PWORD)
             if ku_timestamp:
                 ku_time = ku_timestamp.strftime('%Y-%m-%d %H:%M:%S')
                 if ku_aos_los:
@@ -91,11 +98,19 @@ def tick():
     # to update the time display as needed
     # could use >200 ms, but display gets jerky
     clock.after(200, tick)
+
+# menu callback for copying time to clipboard
+def menu_callback(fmt):
+    r = Tk()
+    r.withdraw()
+    r.clipboard_clear()
+    r.clipboard_append(datetime.datetime.now().strftime(fmt))
+    r.destroy()
     
 if __name__ == "__main__":
     
     # get font size, window width, and window height
-    font_size, w, h = get_config()
+    font_size, w, h, _default, _custom = get_config()
     
     # get root window and screen dimensions
     root = Tk()
@@ -110,10 +125,19 @@ if __name__ == "__main__":
     geom = '%dx%d+%d+%d' % (w, h, x, y)
     root.geometry( geom )
     
+    # create popup menu
+    menu = Menu(root, tearoff=0)
+    menu.add_command(label="CopyDefault", command=lambda: menu_callback(_default))
+    menu.add_command(label="CopyCustom", command=lambda: menu_callback(_custom))   
+    
+    def popup(event):
+        menu.post(event.x_root, event.y_root)    
+    
     # initialize time, color, and clock label before main loop
     time1 = ''
     clock = Label(root, font=('arial', font_size, 'bold'), bg='white')
-    clock.pack(fill=BOTH, expand=1)    
+    clock.pack(fill=BOTH, expand=1)
+    clock.bind("<Button-3>", popup)
     
     # get time now, do first tick, and start main loop
     dt1 = datetime.datetime.now()   
