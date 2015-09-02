0cef8e9b7a6773a3998ed2f6ee6e0c3514edb3db
SilverSnoopy2002 <silversnoopy2002@gmail.com>
2015-09-02 11:28:28 +0000

    Modified largeclock for yoda db connection check.

--- largeclock/db.py
+++ largeclock/db.py
@@ -1,13 +1,25 @@
 from MySQLdb import *
 
-def query_aos(passwd):
-    con = Connection(host='yoda', user='samsops', passwd=passwd, db='samsnew')
-    cursor = con.cursor()
-    cursor.execute('select ku_timestamp, ku_aos_los_status from gse_packet_rt;')
-    results = cursor.fetchall();
-    cursor.close()
-    con.close()
-    ku_timestamp, ku_aos_los = results[0]
-    return ku_timestamp, ku_aos_los
+# FIXME this is where we would want to timeout (with a thread)
 
+# attempt to connect to yoda and get most recent Ku timestamp from gse_packet_rt table
+def query_aos(passwd, out_q):
+    # attempt to get results
+    try:
+        con = Connection(host='yoda', user='samsops', passwd=passwd, db='samsnew')
+        cursor = con.cursor()
+        cursor.execute('select ku_timestamp, ku_aos_los_status from gse_packet_rt;')
+        results = cursor.fetchall();
+        cursor.close()
+        con.close()
+    except:
+        #print 'ERROR IN YODA DB CONNECTION'
+        results = None
+        
+    # check if we have any results (should always just be one)
+    if results:
+        ku_timestamp, ku_aos_los = results[0]
+    else:
+        ku_timestamp, ku_aos_los = None, None
 
+    out_q.put( (ku_timestamp, ku_aos_los) )
--- largeclock/largeclock.py
+++ largeclock/largeclock.py
@@ -10,56 +10,59 @@ import time
 import datetime
 import ConfigParser
 from Tkinter import *
-#from pims.tkclock.db import query_aos
+from multiprocessing import Queue
+
 from pims.largeclock.db import query_aos
+from pims.largeclock.timeout import run_with_timeout
 
-_PWORD = raw_input('samsops pword')
+_PWORD = raw_input('samsops pword: ') # USE TEMPORARY HARD-CODED FOR PY2EXE
 
-def getScriptPath():
+# get path to this script; used to find largeclock.cfg (config) file
+def get_script_path():
     return os.path.dirname(os.path.realpath(sys.argv[0]))
 
 # if no input args, then we try to read cfg file (fallback to hard-coded values)
-if len(sys.argv) == 1:
-    try:
-        # attempt to read local config file
-        script_path = getScriptPath()
-        config_file = os.path.join(script_path, "largeclock.cfg")
-        #config_file = r"C:\\largeclock\\largeclock.cfg"
-        config = ConfigParser.ConfigParser()
-        config.read(config_file)
-        font_size = int( config.get('font', 'size') )
-        w = int( config.get('window', 'width') )
-        h = int( config.get('window', 'height') )
-    except:
-        font_size = 180 # font size
-        w, h = 1500, 200 # width and height of Tk root window
-else:
-    font_size = int(sys.argv[1])
-    w = int(sys.argv[2])
-    h = int(sys.argv[3])
-    
-# get root window and screen dimensions
-root = Tk()
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
+def get_config():
+    if len(sys.argv) == 1:
+        try:
+            # attempt to read local config file
+            script_path = get_script_path()
+            config_file = os.path.join(script_path, "largeclock.cfg")
+            #config_file = r"C:\\largeclock\\largeclock.cfg"
+            config = ConfigParser.ConfigParser()
+            config.read(config_file)
+            font_size = int( config.get('font', 'size') )
+            w = int( config.get('window', 'width') )
+            h = int( config.get('window', 'height') )
+        except:
+            font_size = 180 # font size
+            w, h = 1500, 200 # width and height of Tk root window
+    else:
+        font_size = int(sys.argv[1])
+        w = int(sys.argv[2])
+        h = int(sys.argv[3])
+        
+    return font_size, w, h
 
-# compute x and y coords for root window
-x = (screen_width/2) - (w/2)
-y = (screen_height/2) - (h/2)
-
-# set root window size and position
-geom = '%dx%d+%d+%d' % (w, h, x, y)
-root.geometry( geom )
-
-# initialize time, color, and clock label before main loop
-time1 = ''
-clock = Label(root, font=('arial', font_size, 'bold'), bg='white')
-clock.pack(fill=BOTH, expand=1)
+# get query results for Ku AOS/LOS with timeout
+def get_aos_query_results_with_timeout(_PWORD):
+    timeout_sec = 3
+    out_q = Queue()
+    args = (_PWORD, out_q)
+    kwargs = {}
+    finished = run_with_timeout(query_aos, timeout_sec, args, kwargs)
+    if finished:
+        if out_q.empty():
+            return None, None
+        else:
+            return out_q.get()
+    else:
+        return None, None # hit the timeout
 
 # update every 200 msec
 def tick():
     global time1, dt1
-    # get the current local time from the PC
+    # get the current local time from local machine
     tnow = time
     time2 = tnow.strftime('%j  %H:%M:%S')
     # if time string has changed, update it
@@ -71,19 +74,48 @@ def tick():
         # every 5 seconds check if we are AOS/LOS and
         # update color & title text
         if time2[-1:] in ['0', '5']:
-            ku_timestamp, ku_aos_los = query_aos(_PWORD)
-            ku_time = ku_timestamp.strftime('%Y-%m-%d %H:%M:%S')
-            if ku_aos_los:
-                clock.config(bg='green')
+            #ku_timestamp, ku_aos_los = query_aos(_PWORD)
+            ku_timestamp, ku_aos_los = get_aos_query_results_with_timeout(_PWORD)
+            if ku_timestamp:
+                ku_time = ku_timestamp.strftime('%Y-%m-%d %H:%M:%S')
+                if ku_aos_los:
+                    clock.config(bg='green')
+                else:
+                    clock.config(bg='yellow')
             else:
-                clock.config(bg='yellow')
+                ku_time = "yoda db connection error"
+                clock.config(bg='white')
             root.title( 'ku_timestamp = %s' %  ku_time)
 
     # now call itself every 200 milliseconds
     # to update the time display as needed
     # could use >200 ms, but display gets jerky
     clock.after(200, tick)
-
-dt1 = datetime.datetime.now()   
-tick()
-root.mainloop()
+    
+if __name__ == "__main__":
+    
+    # get font size, window width, and window height
+    font_size, w, h = get_config()
+    
+    # get root window and screen dimensions
+    root = Tk()
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    
+    # compute x and y coords for root window
+    x = (screen_width/2) - (w/2)
+    y = (screen_height/2) - (h/2)
+    
+    # set root window size and position
+    geom = '%dx%d+%d+%d' % (w, h, x, y)
+    root.geometry( geom )
+    
+    # initialize time, color, and clock label before main loop
+    time1 = ''
+    clock = Label(root, font=('arial', font_size, 'bold'), bg='white')
+    clock.pack(fill=BOTH, expand=1)    
+    
+    # get time now, do first tick, and start main loop
+    dt1 = datetime.datetime.now()   
+    tick()
+    root.mainloop()
