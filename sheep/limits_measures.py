c0621437c611ccaf94dfd67d01b1e87645128e2c
Ken <none>
2017-02-27 18:54:02 -0500

    Refined sheep/limits_measures.py.

--- sheep/limits_measures.py
+++ sheep/limits_measures.py
@@ -3,22 +3,22 @@
 # FIXME better way to track limits over time and apply here (span all EEs!)?
 # define y-limits based on measure
 YLIMS = {       
-        'head0_tempX':      [  23,  31 ],
-        'head0_tempY':      [  23,  31 ],
-        'head0_tempZ':      [  23,  31 ],
-        'head1_tempX':      [  23,  31 ],
-        'head1_tempY':      [  23,  31 ],
-        'head1_tempZ':      [  23,  31 ],
-        'tempbase':         [  22,  27 ],
-        'head0_plus5V':     [   5.05,   5.2 ],
+        'head0_tempX':      [  23.00,  31.00 ],
+        'head0_tempY':      [  23.00,  31.00 ],
+        'head0_tempZ':      [  23.00,  31.00 ],
+        'head1_tempX':      [  23.00,  31.00 ],
+        'head1_tempY':      [  23.00,  31.00 ],
+        'head1_tempZ':      [  23.00,  31.00 ],
+        'tempbase':         [  22.00,  27.00 ],
+        'head0_plus5V':     [   5.05,   5.20 ],
         'head0_plus15V':    [  14.75,  15.15 ],
-        'head0_minus15V':   [ -15.25, -14.8 ],
-        'head1_plus5V':     [   5.05,   5.2 ],
+        'head0_minus15V':   [ -15.25, -14.80 ],
+        'head1_plus5V':     [   5.05,   5.20 ],
         'head1_plus15V':    [  14.75,  15.15 ],
-        'head1_minus15V':   [ -15.25, -14.8 ],
-        'pc104_plus5V':     [   5.05,   5.2 ],
-        'ref_plus5V':       [   4.0,    5.0 ],
-        #'ref_zeroV':        [  -1,   1 ],
+        'head1_minus15V':   [ -15.25, -14.80 ],
+        'pc104_plus5V':     [   5.05,   5.20 ],
+        'ref_plus5V':       [   4.00,   5.00 ],
+        #'ref_zeroV':        [  -1.00,   1.00 ],
 }
 
 # define measures to consider for truly tracking trending
