fa490bd61d05d1ab6943a93a8f81151edb6553fe
Ken <none>
2019-01-07 19:13:36 -0500

    Included demo for step plot.

--- otorunhist/demo.py
+++ otorunhist/demo.py
@@ -589,10 +589,35 @@ def demo_look_jury_rig_pickle_file():
     print fat_array.shape
     print fidx.keys()
 
-    pass
+def demo_step_plot():
+
+    x = np.arange(1, 7, 0.4)
+    y0 = np.sin(x)
+    y = y0.copy() + 2.5
+
+    plt.step(x, y, label='pre (default)')
+
+    y -= 0.5
+    plt.step(x, y, where='mid', label='mid')
+
+    y -= 0.5
+    plt.step(x, y, where='post', label='post')
+
+    y = np.ma.masked_where((y0 > -0.15) & (y0 < 0.15), y - 0.5)
+    plt.step(x, y, label='masked (pre)')
+
+    plt.legend()
+
+    plt.xlim(0, 7)
+    plt.ylim(-0.5, 4)
+
+    plt.show()
 
 
 if __name__ == '__main__':
+
+    demo_step_plot()
+    raise SystemExit
 
     # demo_jury_rig_pickle_file()
     # demo_look_jury_rig_pickle_file()
