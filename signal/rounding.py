a55173fe159158b327fce5a1792a6234fb62f87a
kenhro <48067298+kenhro@users.noreply.github.com>
2020-05-07 14:23:40 -0400

    Update rounding.py
    Add function for is_power_of_two.

--- signal/rounding.py
+++ signal/rounding.py
@@ -2,9 +2,19 @@
 
 import math
 
+
+def is_power_of_two(n):
+    """Return True if n is a power of two."""
+    if n <= 0:
+        return False
+    else:
+        return n & (n - 1) == 0
+
+
 def roundup100(x):
 	"""return integer value for x rounded up to next hundred"""
 	return int(math.ceil(x / 100.0)) * 100
+
 
 def roundup_int(x, m):
 	"""return integer value for x rounded up to next multiple of m"""
